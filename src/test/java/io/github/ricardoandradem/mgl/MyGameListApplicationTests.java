45f70163f65407f6ab49a73c42aa1c85a2c93a4a
Ricardo Andrade <ricardo.maia12@gmail.com>
2025-08-25 03:29:01 -0300

    Initial Commit

---
+++ src/test/java/io/github/ricardoandradem/mgl/MyGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ricardoandradem.mgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
